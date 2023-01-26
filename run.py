66de4c8ad32d7dc378d169083cff0f6e1125125e
bernhard <bernhard.pfann@gmail.com>
2023-01-26 15:31:13 +0100

    reworked backtrans_oos method in pca

--- run.py
+++ run.py
@@ -12,11 +12,14 @@ def main():
     if not os.path.exists("assets"):
         os.makedirs("assets")
 
-    download(
-        target_path = "assets/rates_raw.csv",
-        start_date  = conf.start_date,
-        end_date    = conf.end_date
-    )
+    lol = False
+
+    if lol:
+        download(
+            target_path = "assets/rates_raw.csv",
+            start_date  = conf.start_date,
+            end_date    = conf.end_date
+        )
 
     df = clean_rates(
         input_path = "assets/rates_raw.csv", 
@@ -33,15 +36,22 @@ def main():
     df_train = df[df.index < test_date]
     df_test = df[df.index >= test_date]
 
-    mdl = PCA(df, k=conf.n_components)
+    mdl = PCA(df=df_train, k=conf.n_components)
     
+    df_train.to_csv("assets/train.csv")
+    df_test.to_csv("assets/test.csv")
+
     dump(mdl, "assets/pca.joblib")
 
-    pc_scores      = mdl.eig_scores_k
-    pc_vectors     = mdl.eig_vect_k
-    pc_vectors_inv = mdl.eig_vect_inv_k
-    pc_back_trans  = mdl.yields
-    pc_idx         = mdl.idx[:conf.n_components]
+    # pc_scores      = mdl.eig_scores_k
+    # pc_vectors     = mdl.eig_vect_k
+    # pc_vectors_inv = mdl.eig_vect_inv_k
+    # pc_back_trans  = mdl.yields
+    # pc_idx         = mdl.idx[:conf.n_components]
+    
+    # df_oos = mdl.backtrans_oos(df_test)
+
+    import pdb; pdb.set_trace()
     print("DONE")
 
 if __name__ == "__main__":
--- src/pca.py
+++ src/pca.py
@@ -4,120 +4,125 @@ import pandas as pd
 
 class PCA(object):
     """
-    This class derives the principal components of a given data matrix. Based on the datas' covariance matrix (func:pca_cov), 
-    Eigenvectors are calculated (func:pca_eig), which act as coordinate system for PC-scores (func:pca_scores). By only retaining
-    a few of the newly established dimensions, the backtransformation (func:pca_backtrans) into original units creates a lower 
-    dimensional data model. 
+    This class derives the principal components of a given data matrix. Based on 
+    the datas' covariance matrix, Eigenvectors are calculated, which act as coordinate 
+    system for PC-scores. By only retaining a few of the newly established dimensions, 
+    the backtransformation into original units creates lower dimensional data model. 
     """
-    def __init__(self, df, k):
-        """
-        Required parameters:
-        - df: DataFrame()
-        - maturities: List >> Given maturities
-        - k: Int >> Number of retained PCs
-        """
+    def __init__(self, df: pd.DataFrame, k: int):
         self.df = df
-        self.mat = df.columns
+        self.maturities = df.columns
         self.k = k
         
-        self.pca_cov()
-        self.pca_eig()
-        self.pca_scores()
-        self.pca_backtrans()
+        self.get_cov_matrix()
+        self.get_eig_vectors()
+        self.get_eig_scores()
+        self.backtrans()
         
-    def pca_cov(self):
+    def get_cov_matrix(self):
         """
-        This function calculates the covariance matrix of all given maturities to each other over time whole time horizon.
+        Calculates the covariance matrix of all given maturities to each other over time 
+        whole time horizon.
         """
         self.cov_matr = np.array(self.df).T
         self.cov_matr = np.cov(self.cov_matr, bias = True)
         self.cov_matr = pd.DataFrame(
             data    = self.cov_matr, 
-            columns = self.mat, 
-            index   = self.mat)
+            columns = self.maturities, 
+            index   = self.maturities
+        )
         
-        
-    def pca_eig(self):
+    def get_eig_vectors(self):
         """
-        This function calculates the Eigenvectors. By definition these are the vectors that capture the maximum variance of the
-        underlying data, and can be found by minimizing the sum of projection length to the respective vector.
+        Calculates the Eigenvectors. By definition these are the vectors that capture the 
+        maximum variance of the underlying data, and can be found by minimizing the sum of 
+        projection length to the respective vector.
         """
-    
         # Eigen decomposition
         eig = np.linalg.eig(self.cov_matr)
         self.idx = list(["PC_"+str(i) for i in range(1, eig[0].shape[0]+1)])
-
 
         # Eigen values 
         self.eig_vals = pd.DataFrame(eig[0].real, columns = ["eig_val"], index = self.idx)
         self.eig_vals["eig_val_rel"] = self.eig_vals["eig_val"].apply(lambda x: x/self.eig_vals["eig_val"].sum())
         self.eig_vals["eig_val_abs"] = self.eig_vals["eig_val_rel"].cumsum()
 
-        
         # Eigen vectors
-        self.eig_vect = pd.DataFrame(eig[1].real, index = self.mat, columns = self.idx)
+        self.eig_vect = pd.DataFrame(
+            data    = eig[1].real, 
+            index   = self.maturities, 
+            columns = self.idx,
+        )
         self.eig_vect_k = self.eig_vect.iloc[:,:self.k]
-
         
-    def pca_scores(self):
+    def get_eig_scores(self):
         """
-        This function transforms the underlying data into the new dimensionality formed by the Eigenvectors. Transformed datapoints
-        can be labeled PC-scores.
+        This function transforms the underlying data into the new dimensionality formed by the 
+        Eigenvectors. Transformed datapointscan be labeled PC-scores.
         """
-    
         # PC scores (all)
         self.eig_scores = np.matrix(self.df) * np.matrix(self.eig_vect)
-        self.eig_scores = pd.DataFrame(data    = self.eig_scores,
-                                       columns = self.idx,
-                                       index   = pd.to_datetime(self.df.index))
+        self.eig_scores = pd.DataFrame(
+            data    = self.eig_scores,
+            columns = self.idx,
+            index   = pd.to_datetime(self.df.index)
+        )
 
         # PC scores (retained)
         self.eig_scores_k = self.eig_scores.iloc[:,:self.k]
-
         
-    def pca_backtrans(self):
+    def backtrans(self):
         """
-        This function retains only a limited set of PCs and transformes the PC-scores back to the original coordinate system. Therefore the
-        final output is in the same units as the input.
+        This function retains only a limited set of PCs and transformes the PC-scores back to the 
+        original coordinate system. Therefore the final output is in the same units as the input.
         """
         
         # Inverse transformation (all)
-        self.eig_vect_inv = pd.DataFrame (data = np.linalg.inv(np.matrix(self.eig_vect)),
-                                          columns = self.mat,
-                                          index = self.idx)
+        self.eig_vect_inv = pd.DataFrame(
+            data    = np.linalg.inv(np.matrix(self.eig_vect)),
+            columns = self.maturities,
+            index   = self.idx)
 
         # Inverse transformation (retained)
         self.eig_vect_inv_k = self.eig_vect_inv.iloc[:self.k,:]
 
         self.yields = np.matrix(self.eig_scores_k) * np.matrix(self.eig_vect_inv_k)
-        self.yields = pd.DataFrame(data    = self.yields, 
-                                   columns = self.mat,
-                                   index   = self.eig_scores_k.index)
+        self.yields = pd.DataFrame(
+            data    = self.yields, 
+            columns = self.maturities,
+            index   = self.eig_scores_k.index)
 
-
-    def pca_oos(self, eig_vect_train, df_test):
+    def backtrans_oos(self, df_oos):
         """
-        This function derives the dimensionality reduction of out-of-sample data. This means, that Eigenvectors are fitted on train data,
-        and are being applied to unseen test data.
+        This function derives the dimensionality reduction of out-of-sample data. This means, that 
+        Eigenvectors are fitted on train data, and are being applied to unseen test data.
         """
         # PC scores (all)
-        eig_scores_oos = np.matrix(df_test) * np.matrix(eig_vect_train)
-        eig_scores_oos = pd.DataFrame(data    = eig_scores_oos,
-                                      columns = self.idx,
-                                      index   = pd.to_datetime(df_test.index))
+        eig_scores = np.matrix(df_oos) * np.matrix(self.eig_vect)
+        eig_scores = pd.DataFrame(
+            data    = eig_scores,
+            columns = self.idx,
+            index   = df_oos.index
+        )
 
         # PC scores (retained)
-        eig_scores_oos_k = eig_scores_oos.iloc[:,:self.k]
+        eig_scores_k = eig_scores.iloc[:,:self.k]
         
         # Inverse transformation (all)
-        eig_vect_inv_oos = pd.DataFrame(data=np.linalg.inv(np.matrix(eig_vect_train)), columns=self.mat, index=self.idx)
+        eig_vect_inv = pd.DataFrame(
+            data    = np.linalg.inv(np.matrix(self.eig_vect)), 
+            columns = self.maturities, 
+            index   = self.idx
+        )
 
         # Inverse transformation (retained)
-        eig_vect_inv_oos_k = eig_vect_inv_oos.iloc[:self.k,:]
+        eig_vect_inv_k = eig_vect_inv.iloc[:self.k,:]
 
-        yields_oos = np.matrix(eig_scores_oos_k) * np.matrix(eig_vect_inv_oos_k)
-        yields_oos = pd.DataFrame(data = yields_oos, 
-                                  columns = self.mat,
-                                  index = eig_scores_oos_k.index)
+        yields = np.matrix(eig_scores_k) * np.matrix(eig_vect_inv_k)
+        yields = pd.DataFrame(
+            data    = yields, 
+            columns = self.maturities,
+            index   = eig_scores_k.index
+        )
         
-        return yields_oos
+        return yields
